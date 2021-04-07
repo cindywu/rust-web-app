1bb78b6f11079a6c10a02785a6828a131f78fce4
cindy <ciindy.wu@gmail.com>
2021-04-07 11:12:16 -1000

    complex templates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use actix_web::{HttpServer, App, web, HttpResponse, Responder};
 use tera::{Tera, Context};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Post {
+    title: String,
+    link: String,
+    author: String,
+}
 
 async fn index(tera: web::Data<Tera>) -> impl Responder {
     let mut data = Context::new();
-    data.insert("title", "Hello Denny!");
-    data.insert("name", "Denny");
+
+    let posts = [
+        Post {
+            title: String::from("Paul Halmos: in his own words"),
+            link: String::from("https://example.com"),
+            author: String::from("John Ewing")
+        },
+        Post {
+            title: String::from("Map location and dimensional definition of subsurface caverns"),
+            link: String::from("https://example.com"),
+            author: String::from("James E. Wolfe")
+        },
+    ];
+
+    data.insert("title", "Hacker Clone");
+    data.insert("posts", &posts);
 
     let rendered = tera.render("index.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)
