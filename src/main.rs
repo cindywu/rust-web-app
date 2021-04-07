a3eac75eac67d4e6402caef6aa088468b310db84
cindy <ciindy.wu@gmail.com>
2021-04-07 11:01:01 -1000

    add templating engine tera, update title and message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use actix_web::{HttpServer, App, web, HttpResponse};
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+    data.insert("title", "Hello Denny!");
+    data.insert("name", "Denny");
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
         App::new()
-            .route("/", web::to(|| {
-                HttpResponse::Ok().body("Hello World!")
-            }))
+            .data(tera)
+            .route("/", web::get().to(index))
     })
     .bind("127.0.0.1:8000")?
     .run()
